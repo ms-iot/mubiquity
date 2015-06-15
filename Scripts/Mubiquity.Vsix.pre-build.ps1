3af31b5a55fa1d6daabf06622aae30a74e9af057
windowsondevices <lamadio@microsoft.com>
2015-06-15 12:02:55 -0700

    Add scripts for autogenerating Files
    Including the Nuget package and Project template

---
+++ Scripts/Mubiquity.Vsix.pre-build.ps1
@@ -0,0 +1,18 @@
+param([string]$directoryToZip="directoryToZip", [string]$zipFile="zipFile", [string]$nugetDirectory)
+
+Add-Type -Assembly System.IO.Compression.FileSystem
+
+# only create the zip if it doesn't exist.
+# Changing the template requires rebuild.
+# TODO: figure out how to make this happen if the contents change.
+if (!(Test-Path -Path $zipFile))
+{
+	[System.IO.Compression.ZipFile]::CreateFromDirectory($directoryToZip, $zipFile);
+}
+
+#assumes nuget.exe is in the path
+Push-Location $nugetDirectory
+$nugetCommand = Join-Path $nugetDirectory "build-nupkg.cmd"
+cmd.exe /c "`"$nugetCommand`""
+Pop-Location
+
